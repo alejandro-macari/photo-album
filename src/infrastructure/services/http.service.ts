44386de168b7ca166f2bf4ad47f8e34cd2072803
Macari <alejandro@macari.dev>
2023-07-27 19:01:41 +0200

    infra: http service

---
+++ src/infrastructure/services/http.service.ts
@@ -0,0 +1,58 @@
+const defaultHeaders = {
+  'Content-Type': 'application/json',
+};
+
+const get = async <T>(
+  url: string,
+  headers: { [key: string]: string } = defaultHeaders,
+) => {
+  const response = await fetch(url, {
+    method: 'GET',
+    headers,
+  });
+  return (await response.json()) as T;
+};
+
+const post = async <T>(
+  url: string,
+  body: any,
+  headers: { [key: string]: string } = defaultHeaders,
+) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers,
+    body,
+  });
+  return (await response.json()) as T;
+};
+
+const put = async <T>(
+  url: string,
+  body: any,
+  headers: { [key: string]: string } = defaultHeaders,
+) => {
+  const response = await fetch(url, {
+    method: 'PUT',
+    headers,
+    body,
+  });
+  return (await response.json()) as T;
+};
+
+const _delete = async <T>(
+  url: string,
+  headers: { [key: string]: string } = defaultHeaders,
+) => {
+  const response = await fetch(url, {
+    method: 'DELETE',
+    headers,
+  });
+  return (await response.json()) as T;
+};
+
+export const http = {
+  get,
+  post,
+  put,
+  delete: _delete,
+};
