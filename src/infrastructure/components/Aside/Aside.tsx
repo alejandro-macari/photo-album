c80ee0d3bdb0fe0801a45a3662e45c7b75876f7d
Macari <alejandro@macari.dev>
2023-07-29 20:54:42 +0200

    feat: aside component

---
+++ src/infrastructure/components/Aside/Aside.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import { Album } from '@/domain/models/album.model';
+import styles from './Aside.module.css';
+
+interface Props {
+  album: Album;
+}
+
+function Aside({ album }: Props) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.image}>
+        <Image
+          height={600}
+          width={600}
+          src={album.cover as string}
+          alt={`${album.title} cover`}
+        />
+      </div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Album title: {album.title}</h1>
+        <p className={styles.subtitle}>Album id: {album.id}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Aside;
