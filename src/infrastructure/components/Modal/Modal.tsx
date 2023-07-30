a8f3a78ec7a9dd7417bf104b705220b262f1c33b
Macari <alejandro@macari.dev>
2023-07-30 19:40:46 +0200

    refactor: internal ref

--- src/infrastructure/components/Modal/Modal.tsx
+++ src/infrastructure/components/Modal/Modal.tsx
@@ -9,14 +9,14 @@ interface Props {
 
 const Modal = ({ close, children }: Props) => {
   const ref = useRef<HTMLElement | null>(null);
-  const ref2 = useRef<any>(null);
+  const internalRef = useRef<any>(null);
   if (!ref.current) {
     ref.current = document.createElement('div');
   }
 
   useEffect(() => {
     const handleClick = (e: any) => {
-      if (ref2.current && !ref2.current.contains(e.target)) {
+      if (internalRef.current && !internalRef.current.contains(e.target)) {
         close();
       }
     };
@@ -29,9 +29,9 @@ const Modal = ({ close, children }: Props) => {
       modalRoot.removeChild(ref.current as HTMLElement);
       document.removeEventListener('click', handleClick, true);
     };
-  }, [ref2]);
+  }, [close, internalRef]);
 
-  return createPortal(<div ref={ref2}>{children}</div>, ref.current);
+  return createPortal(<div ref={internalRef}>{children}</div>, ref.current);
 };
 
 export default Modal;
