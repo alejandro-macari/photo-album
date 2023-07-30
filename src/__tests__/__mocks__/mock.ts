bff69811619b5563b743fdea2ab734116c4abd20
Macari <alejandro@macari.dev>
2023-07-30 21:50:22 +0200

    test: adding mocks

---
+++ src/__tests__/__mocks__/mock.ts
@@ -0,0 +1,26 @@
+const mockAlbum = {
+  userId: 1,
+  id: 1,
+  title: 'quidem molestiae enim',
+  cover: 'https://via.placeholder.com/600/92c952',
+  coverThumbnail: 'https://via.placeholder.com/150/92c952',
+};
+
+const mockAlbumList = [mockAlbum];
+
+const mockPhoto = {
+  albumId: 1,
+  id: 1,
+  title: 'accusamus beatae ad facilis cum similique qui sunt',
+  url: 'https://via.placeholder.com/600/92c952',
+  thumbnailUrl: 'https://via.placeholder.com/150/92c952',
+};
+
+const mockPhotoList = [mockPhoto];
+
+export {
+    mockAlbum,
+    mockAlbumList,
+    mockPhoto,
+    mockPhotoList
+}
