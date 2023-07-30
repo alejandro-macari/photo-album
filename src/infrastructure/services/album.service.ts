229dd2d50b98ff0cabbd13d0e19cd329593b725a
Macari <alejandro@macari.dev>
2023-07-30 12:46:20 +0200

    feat: adding cache functionality

--- src/infrastructure/services/album.service.ts
+++ src/infrastructure/services/album.service.ts
@@ -6,21 +6,58 @@ import { Photo } from '@/domain/models/photo.model';
 import { AlbumRepository } from '@/domain/repositories/album.repo';
 
 const _BASE_URL = 'https://jsonplaceholder.typicode.com/albums';
+const _ALBUMS_KEY = "albums"
+
+const headers = {
+  'Content-Type': 'application/json',
+  'next': {
+    'revalidate': 24 * 60 * 60
+  }
+}
+
+interface Cache {
+    data: { [id: string]: Album },
+    timestamp: number
+}
+
+const getDataFromStorage = (): Cache | null => {
+  const storage = localStorage.getItem(_ALBUMS_KEY);
+  if (storage) {
+    return JSON.parse(storage) as Cache
+  }
+  return null
+}
+
+const setDataToStorage = (albums: Album[]) => {
+  localStorage.setItem(_ALBUMS_KEY, JSON.stringify({
+    data: albums.reduce((prev, curr) => ({...prev, [curr.id]: curr }), {}),
+    timestamp: new Date().getTime()
+  }))
+}
 
 const get = async (): Promise<Album[]> => {
-  const albums = (await http.get(_BASE_URL)) as Album[];
-  const albumIds = albums.map((album) => album.id);
-  const covers = (await photoService.getById(albumIds)) as Photo[];
-  for (let i = 0; i < albums.length; i++) {
-    const { url, thumbnailUrl } = covers[i];
-    albums[i].cover = url;
-    albums[i].coverThumbnail = thumbnailUrl;
+  const cache = getDataFromStorage();
+  const now = new Date().getTime()
+
+  if (!cache || Object.values(cache.data).length < 100 || now - cache.timestamp > 24 * 60 * 60 * 1000) {
+    console.log("fetchin")
+    const albums = (await http.get(_BASE_URL)) as Album[];
+    const albumIds = albums.map((album) => album.id);
+    const covers = (await photoService.getById(albumIds)) as Photo[];
+    for (let i = 0; i < albums.length; i++) {
+      const { url, thumbnailUrl } = covers[i];
+      albums[i].cover = url;
+      albums[i].coverThumbnail = thumbnailUrl;
+    }
+    setDataToStorage(albums)
+    return albums;
   }
-  return albums;
+  
+  return Object.values(cache.data);
 };
 
 const getById = async (albumId: number) => {
-  const album = (await http.get(`${_BASE_URL}/${albumId}`)) as Album;
+  const album = (await http.get(`${_BASE_URL}/${albumId}`, headers)) as Album;
   const photo = await photoService.getAlbumCover(albumId);
   return {
     ...album,
--- src/infrastructure/services/photo.service.ts
+++ src/infrastructure/services/photo.service.ts
@@ -4,25 +4,32 @@ import { PhotoRepository } from '@/domain/repositories/photo.repo';
 
 const _BASE_URL = 'https://jsonplaceholder.typicode.com/photos';
 
+const headers = {
+  'Content-Type': 'application/json',
+  'next': {
+    'revalidate': 24 * 60 * 60
+  }
+}
+
 const getById = async (
   photoId: number | number[],
 ): Promise<Photo | Photo[]> => {
   if (Array.isArray(photoId)) {
     const query = photoId.map((id) => `id=${id}`).join('&');
-    return (await http.get(`${_BASE_URL}?${query}`)) as Photo[];
+    return (await http.get(`${_BASE_URL}?${query}`, headers)) as Photo[];
   }
-  return (await http.get(`${_BASE_URL}/${photoId}`)) as Photo;
+  return (await http.get(`${_BASE_URL}/${photoId}`, headers)) as Photo;
 };
 
 const getByAlbum = async (albumId: number): Promise<Photo[]> => {
-  return (await http.get(`${_BASE_URL}?albumId=${albumId}`)) as Photo[];
+  return (await http.get(`${_BASE_URL}?albumId=${albumId}`, headers)) as Photo[];
 };
 
 const getAlbumCover = async (albumId: number): Promise<Photo> => {
   // Note: this calculation is only possible as all albums have 50 photos
   // in a non-standard case we would have to query all the albums photos and take the first one
   const photoId = (albumId - 1) * 50 + 1;
-  return (await http.get(`${_BASE_URL}/${photoId}`)) as Photo;
+  return (await http.get(`${_BASE_URL}/${photoId}`, headers)) as Photo;
 };
 
 export const photoService = {
