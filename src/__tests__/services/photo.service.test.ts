497a5856e71d86d3dbb77eac7cd8e5794dd7b127
Macari <alejandro@macari.dev>
2023-07-30 21:52:27 +0200

    test: services

---
+++ src/__tests__/services/photo.service.test.ts
@@ -0,0 +1,20 @@
+import { photoService } from '@/infrastructure/services/photo.service';
+import { mockPhoto, mockPhotoList } from '../__mocks__/mock';
+
+test('Should return a list of photos', async () => {
+    jest.spyOn(photoService, 'getByAlbum').mockResolvedValue(mockPhotoList);
+    const albums = await photoService.getByAlbum(1)
+    expect(albums).toBe(mockPhotoList)
+});
+
+test('Should return a list of photos', async () => {
+    jest.spyOn(photoService, 'getAlbumCover').mockResolvedValue(mockPhoto);
+    const albums = await photoService.getAlbumCover(1)
+    expect(albums).toBe(mockPhoto)
+});
+
+test('Should return a list of photos', async () => {
+    jest.spyOn(photoService, 'getById').mockResolvedValue(mockPhoto);
+    const albums = await photoService.getById(1)
+    expect(albums).toBe(mockPhoto)
+});
