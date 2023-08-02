3d0c2027cd77b5f71c798adf53e530fd05f1bd88
Alejandro Macari <alejandro@macari.dev>
2023-08-02 19:39:13 +0200

    fix: typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Photo album application',
-  description: 'Technical assesment',
+  description: 'Technical assessment',
 };
 
 export default function RootLayout({
